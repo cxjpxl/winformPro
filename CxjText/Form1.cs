50800222a1b769df5df0316be1a805a41f1daf33
cxjpxl <81886404@qq.com>
2017-11-06 12:03:48 +0800

    添加云打码余额检测处理

--- CxjText/Form1.cs
+++ CxjText/Form1.cs
@@ -1,7 +1,7 @@
 ﻿using CxjText.utils;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CxjText
@@ -34,6 +34,63 @@ namespace CxjText
            
         }
 
+        //去登录验证
+        private void goLogin(object obj) {
+            JObject jObject = (JObject)obj;
+            String codeUserStr =(String) jObject["codeUserStr"];
+            String codePwdStr = (String)jObject["codePwdStr"];
+            //登录云打码账号
+            int uid = utlis.YDMWrapper.YDM_Login(codeUserStr, codePwdStr);
+            if (uid < 0) {
+                Invoke(new Action(() =>
+                {
+                    loginSysBtn.Enabled = true;
+                    loginSysBtn.Text = "登录";
+                    MessageBox.Show("登录云打码账号失败！");
+                }));
+                return;
+            }
+            //获取云代码账号金额
+            int codeMoney = utlis.YDMWrapper.YDM_GetBalance(codeUserStr,codePwdStr);
+            if (codeMoney <= 0) {
+                Invoke(new Action(() =>
+                {
+                    loginSysBtn.Enabled = true;
+                    loginSysBtn.Text = "登录";
+                    if (codeMoney == -1007)
+                    {
+                        MessageBox.Show("云打码账户余额不足,请充值！");
+                    }
+                    else {
+                        MessageBox.Show("登录云打码账号失败！");
+                    }
+                    
+                }));
+                return;
+            }
+
+            if (codeMoney < 10) {
+                Invoke(new Action(() =>
+                {
+                    loginSysBtn.Enabled = true;
+                    loginSysBtn.Text = "登录";
+                    MessageBox.Show("云打码余额快不足,请充值使用！");
+                }));
+                return;
+            }
+           
+            //登录成功
+            Invoke(new Action(() =>
+            {
+                Config.codeUserStr = codeUserStr;
+                Config.codePwdStr = codePwdStr;
+                MainFrom mainFrom = new MainFrom();
+                mainFrom.Show();
+                this.Hide();
+            }));
+        }
+
+
         //登录系统按键
         private void loginSysBtn_Click(object sender, EventArgs e)
         {
@@ -46,23 +103,12 @@ namespace CxjText
             //登录打码平台并记录在程序里面  并显示软件使用时间  时间优先级比较高  同时客户端记录软件使用时间
             loginSysBtn.Text = "登录中，请耐心等待!";
             loginSysBtn.Enabled = false;
-            int uid = utlis.YDMWrapper.YDM_Login(codeUserStr, codePwdStr);
-            Config.console("uid:"+uid);
-            if (uid > 0)
-            {
-                Config.codeUserStr = codeUserStr;
-                Config.codePwdStr = codePwdStr;
-                MainFrom mainFrom = new MainFrom();
-                mainFrom.Show();
-                this.Hide();
-            }
-            else {
-                loginSysBtn.Enabled = true;
-                loginSysBtn.Text = "登录";
-                MessageBox.Show("登录云打码账号失败！");
-                return;
-            }
 
+            JObject jobject = new JObject();
+            jobject.Add("codeUserStr", codeUserStr);
+            jobject.Add("codePwdStr", codePwdStr);
+            Thread t = new Thread(new ParameterizedThreadStart(goLogin));
+            t.Start(jobject);
         }
     }
 }
