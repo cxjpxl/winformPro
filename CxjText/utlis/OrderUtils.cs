53fd46a1f023c6f8e72541110f1b56a6d35c4aa1
cxjpxl <81886404@qq.com>
2017-11-13 22:37:17 +0800

    添加打印

--- CxjText/utlis/OrderUtils.cs
+++ CxjText/utlis/OrderUtils.cs
@@ -21,10 +21,13 @@ namespace CxjText.utlis
             UserInfo user = (UserInfo)Config.userList[index];
             //请求发出前先更新UI 标记http请求已发送
             JObject headJObject = new JObject();
+            Console.WriteLine(parmsStr);
             headJObject["Origin"] = user.dataUrl;
+            headJObject["Referer"] = user.dataUrl + "Referer:https://a58hg.lq2222.org/sport/football.aspx?action=re&uid=" + user.uid;
+            headJObject["Host"] = user.dataUrl;
             String orderUrl = user.dataUrl + "/sport/order_ft.aspx?uid=" + user.uid;
             String rlt = HttpUtils.HttpPostHeader(orderUrl, parmsStr, "application/x-www-form-urlencoded; charset=UTF-8", user.cookie, headJObject);
-            //Console.WriteLine(rlt);
+            Console.WriteLine(rlt);
             if (rlt == null || rlt.IndexOf("false") >= 0 || rlt.Length > 0)
             {
 
