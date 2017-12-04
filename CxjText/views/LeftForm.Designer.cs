6a1c729c384d26c9f4c6967d094b8116dd58a6ec
cxjpxl <81886404@qq.com>
2017-12-04 22:32:25 +0800

    修改颜色值

--- CxjText/views/LeftForm.Designer.cs
+++ CxjText/views/LeftForm.Designer.cs
@@ -31,9 +31,9 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.nameShowGridView = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.datapanel = new System.Windows.Forms.Panel();
             this.rltPanle = new System.Windows.Forms.Panel();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.nameShowGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,7 +42,7 @@
             this.nameShowGridView.AllowUserToAddRows = false;
             this.nameShowGridView.AllowUserToResizeColumns = false;
             this.nameShowGridView.AllowUserToResizeRows = false;
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(166)))), ((int)(((byte)(112)))));
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.LemonChiffon;
             this.nameShowGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.nameShowGridView.BackgroundColor = System.Drawing.SystemColors.Control;
             this.nameShowGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -52,13 +52,24 @@
             this.nameShowGridView.Margin = new System.Windows.Forms.Padding(0);
             this.nameShowGridView.Name = "nameShowGridView";
             this.nameShowGridView.RowHeadersVisible = false;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(249)))), ((int)(((byte)(206)))));
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.nameShowGridView.RowsDefaultCellStyle = dataGridViewCellStyle2;
             this.nameShowGridView.RowTemplate.Height = 23;
             this.nameShowGridView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.nameShowGridView.Size = new System.Drawing.Size(152, 630);
             this.nameShowGridView.TabIndex = 0;
             this.nameShowGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.NameShowGridView_CellMouseClick);
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.name.DataPropertyName = "name";
+            this.name.HeaderText = "系统球赛";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            this.name.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.name.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.name.Width = 150;
             // 
             // datapanel
             // 
@@ -77,17 +88,6 @@
             this.rltPanle.Name = "rltPanle";
             this.rltPanle.Size = new System.Drawing.Size(815, 154);
             this.rltPanle.TabIndex = 4;
-            // 
-            // name
-            // 
-            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
-            this.name.DataPropertyName = "name";
-            this.name.HeaderText = "系统球赛";
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
-            this.name.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.name.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            this.name.Width = 150;
             // 
             // LeftForm
             // 
