307944d90b3b0b6e3f75c10209d374e569f2dc36
cxjpxl <81886404@qq.com>
2017-12-04 22:43:21 +0800

    提交代码

--- CxjText/utlis/WebSocketUtils.cs
+++ CxjText/utlis/WebSocketUtils.cs
@@ -32,7 +32,7 @@ namespace CxjText.utlis
             if (contentStatus!=1) return;
 
 
-            if (FormUtils.getCurrentTime() - time > 1000 * 60) //1分钟收不到立马重连
+            if (FormUtils.getCurrentTime() - time > 1000 * 60*5) //1分钟收不到立马重连
             { //5分钟没有收到数据
                 contentStatus = 0;
                 isError = true;
