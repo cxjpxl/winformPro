74dfdacdda99b799639a40749860bb3a505371be
[cxjpxl] <[81886404@qq.com]>
2018-03-29 14:59:53 +0800

    添加错误日志捕捉

--- CxjText/Program.cs
+++ CxjText/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CxjText
 {
@@ -24,38 +23,77 @@ namespace CxjText
                 //处理非UI线程异常   
                 AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
+
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
+
+
                 Application.Run(new Form1());
             }
-            catch (Exception e) {
-                Console.WriteLine("主程序里面\n"+e.ToString());
-            }
-        }
-        
-
-        ///<summary>
-        ///  这就是我们要在发生未处理异常时处理的方法，我这是写出错详细信息到文本，如出错后弹出一个漂亮的出错提示窗体，给大家做个参考
-        ///  做法很多，可以是把出错详细信息记录到文本、数据库，发送出错邮件到作者信箱或出错后重新初始化等等
-        ///  这就是仁者见仁智者见智，大家自己做了。
-        ///</summary>
-        ///<param name="sender"> </param>
-        ///<param name="e"> </param>
-        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
-        {
-            var ex = e.Exception;
-            if (ex != null)
+            catch (Exception ex)
             {
-                Console.WriteLine("主程序线程崩溃:" + ex.ToString());
+                var strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now + "\r\n";
+
+                var str = string.Format(strDateInfo + "异常类型：{0}\r\n异常消息：{1}\r\n异常信息：{2}\r\n",
+                                           ex.GetType().Name, ex.Message, ex.StackTrace);
+
+                WriteLog(str);
+                MessageBox.Show("发生错误，请查看程序日志！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
             }
         }
 
-        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        /// <summary>
+        ///错误弹窗
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
         {
-            var ex = e.ExceptionObject as Exception;
-            if (ex != null)
+            string str;
+            var strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now + "\r\n";
+            var error = e.Exception;
+            if (error != null)
             {
-                Console.WriteLine("主程序Ui崩溃:" + ex.ToString());
+                str = string.Format(strDateInfo + "异常类型：{0}\r\n异常消息：{1}\r\n异常信息：{2}\r\n",
+                     error.GetType().Name, error.Message, error.StackTrace);
+            }
+            else
+            {
+                str = string.Format("应用程序线程错误:{0}", e);
+            }
+
+            WriteLog(str);
+            MessageBox.Show("发生错误，请查看程序日志！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Environment.Exit(0);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var error = e.ExceptionObject as Exception;
+            var strDateInfo = "出现应用程序未处理的异常：" + DateTime.Now + "\r\n";
+            var str = error != null ? string.Format(strDateInfo + "Application UnhandledException:{0};\n\r堆栈信息:{1}", error.Message, error.StackTrace) : string.Format("Application UnhandledError:{0}", e);
+
+            WriteLog(str);
+            MessageBox.Show("发生错误，请查看程序日志！", "系统错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Environment.Exit(0);
+        }
+        /// <summary>
+        /// 写文件
+        /// </summary>
+        /// <param name="str"></param>
+        static void WriteLog(string str)
+        {
+            if (!Directory.Exists("ErrLog"))
+            {
+                Directory.CreateDirectory("ErrLog");
+            }
+
+            using (var sw = new StreamWriter(@"ErrLog\ErrLog.txt", true))
+            {
+                sw.WriteLine(str);
+                sw.WriteLine("---------------------------------------------------------");
+                sw.Close();
             }
         }
     }
